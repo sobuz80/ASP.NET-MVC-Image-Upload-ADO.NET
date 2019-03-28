0ffff0b0d3f693d17be269fbee29b0ce81b3b7c9
sobuz80 <sobuz80@gmail.com>
2019-03-28 22:08:04 +0600

    initial commit

---
+++ UploadImageMvc/Controllers/ImageController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UploadImageMvc.Models;
+using System.IO;
+
+namespace UploadImageMvc.Controllers
+{
+    public class ImageController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Image image)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(image.ImageFile.FileName);
+            string extension = Path.GetExtension(image.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssff") + extension;
+            image.ImagePath = "~/Image/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+            image.ImageFile.SaveAs(fileName);
+            using (DBModels db = new DBModels())
+            {
+                db.Images.Add(image);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            Image image = new Image();
+            using (DBModels db = new DBModels())
+            {
+                image = db.Images.Where(x => x.ImageID == id).FirstOrDefault();
+
+            }
+
+                return View(image);
+        }
+
+
+        }
+}
